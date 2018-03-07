b2d323ff45bea1a150c4b6d5fa42b33402ec03e0
rubenpve <rverdugo@travix.com>
2018-03-07 09:57:17 +0000

    Add preset importer contract

---
+++ src/main/java/com/rubasace/bias/preset/manager/service/PresetImporter.java
@@ -0,0 +1,20 @@
+package com.rubasace.bias.preset.manager.service;
+
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+
+@Service
+public class PresetImporter {
+
+  public void addAll(final File biasDirectory, final String bankFolder, final File presetsDirectory) {
+
+    //readAllFiles X
+    //convertFiles to preset.java X
+    //read preset.json -> PresetCollection.java
+    //add presets to PresetCollection
+    //write presetCollection
+    //copy files
+
+  }
+}
