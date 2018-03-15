3125ac87d7bf554223e13ab896fb32ad1b44e363
rubenpve <rverdugo@travix.com>
2018-03-15 22:49:54 +0000

    Fix Context startup test

--- bias-fx-preset-manager-app/src/test/java/com/rubasace/bias/preset/manager/app/ManagerApplicationTest.java
+++ bias-fx-preset-manager-app/src/test/java/com/rubasace/bias/preset/manager/app/ManagerApplicationTest.java
@@ -1,20 +1,17 @@
 package com.rubasace.bias.preset.manager.app;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = {ManagerApplication.class},
-                webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ManagerApplicationTest {
 
     //TODO investigate why test fails with ClassDefNotFoundException
-    @Ignore
     @Test
-    public void shouldStartup() {
+    public void contextLoads() {
         //Nothing to do, just load the context
     }
 }
